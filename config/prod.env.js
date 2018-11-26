ffc8abccac1c223ae3d20a3b8321c95549671d7a
incora-dev <dev@incorainc.com>
2018-11-27 00:29:11 +0200

    add trailing slash to prod url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  FORM_API_URL: '"http://oscargibson.pythonanywhere.com/secret"',
+  FORM_API_URL: '"http://oscargibson.pythonanywhere.com/secret/"',
 }
